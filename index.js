4b4ae26961475881c875a6589d2c94de58900a0c
Totemi1324 <nemestomi2@gmail.com>
2023-06-11 01:27:39 +0200

    Add and configure basic HTML structure

---
+++ index.js
@@ -0,0 +1,58 @@
+// Entry-point for HTML5 canvas application
+// (c) Tamas Nemes, 2022
+// DO NOT TOUCH THIS FILE!
+
+import { main, clear } from "./main.js";
+
+const defaultFrameRate = 64;
+var fpsInterval, now, then, elapsed;
+
+// Set framerate
+function setFramesPerSecond(fps) {
+    fpsInterval = 1000 / fps;
+    then = window.performance.now();
+}
+
+// Update instances
+function update() {
+    let instances = main();
+    instances.forEach((instance) => {
+        if (typeof instance.update == "function") {
+            instance.update();
+        }
+    });
+}
+
+// App structure
+function myApp() {
+    setup();
+    loop();
+}
+
+// One-time setup functions
+function setup() {
+    setFramesPerSecond(defaultFrameRate);
+}
+
+// Main loop
+function loop() {
+    now = window.performance.now();
+    elapsed = now - then;
+
+    if (elapsed > fpsInterval) {
+        then = now - (elapsed % fpsInterval);
+
+        clear();
+
+        try {
+            update();
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    requestAnimationFrame(loop);
+}
+
+// Run app
+myApp();
