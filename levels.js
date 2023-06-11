8134e9a1b298ddd92790a8914a6816cfbe40abac
Totemi1324 <nemestomi2@gmail.com>
2023-06-11 22:08:41 +0200

    Add static stores

---
+++ levels.js
@@ -0,0 +1,20 @@
+export const LEVELS = {
+    levels: [
+        {
+            width: 4,
+            height: 4,
+            startPositions: [
+                [0, 0],
+                [0, 1],
+                [1, 0]
+            ],
+            finishPosition: [3, 3],
+            holePositions: [
+                [1, 1],
+                [3, 1],
+                [3, 2],
+                [0, 3],
+            ],
+        },
+    ],
+};
