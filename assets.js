828c148cd4c63a4154e88638fa1f196f62587af9
Totemi1324 <nemestomi2@gmail.com>
2023-06-13 00:40:56 +0200

    Correct assets path

--- assets.js
+++ assets.js
@@ -1,11 +1,11 @@
 export const ASSETS = {
-    agent: "./assets/images/agent_tile.png",
-    finish: "./assets/images/finish_tile.png",
-    default: "./assets/images/snow_tile.png",
-    hole: "./assets/images/hole_tile.png",
-    reward: "./assets/images/reward_tile.png",
-    arrowUp: "./assets/images/arrow_up.png",
-    arrowDown: "./assets/images/arrow_down.png",
-    arrowLeft: "./assets/images/arrow_left.png",
-    arrowRight: "./assets/images/arrow_right.png"
-}
+    agent: "/q-learning-demo/assets/images/agent_tile.png",
+    finish: "/q-learning-demo/assets/images/finish_tile.png",
+    default: "/q-learning-demo/assets/images/snow_tile.png",
+    hole: "/q-learning-demo/assets/images/hole_tile.png",
+    reward: "/q-learning-demo/assets/images/reward_tile.png",
+    arrowUp: "/q-learning-demo/assets/images/arrow_up.png",
+    arrowDown: "/q-learning-demo/assets/images/arrow_down.png",
+    arrowLeft: "/q-learning-demo/assets/images/arrow_left.png",
+    arrowRight: "/q-learning-demo/assets/images/arrow_right.png",
+};
