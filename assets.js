83920d862699b800e7bee23545e9eabd91f3177e
Totemi1324 <nemestomi2@gmail.com>
2023-06-13 00:38:19 +0200

    Correct assets path

--- assets.js
+++ assets.js
@@ -1,11 +1,11 @@
 export const ASSETS = {
-    agent: "/assets/images/agent_tile.png",
-    finish: "/assets/images/finish_tile.png",
-    default: "/assets/images/snow_tile.png",
-    hole: "/assets/images/hole_tile.png",
-    reward: "/assets/images/reward_tile.png",
-    arrowUp: "/assets/images/arrow_up.png",
-    arrowDown: "/assets/images/arrow_down.png",
-    arrowLeft: "/assets/images/arrow_left.png",
-    arrowRight: "/assets/images/arrow_right.png"
+    agent: "./assets/images/agent_tile.png",
+    finish: "./assets/images/finish_tile.png",
+    default: "./assets/images/snow_tile.png",
+    hole: "./assets/images/hole_tile.png",
+    reward: "./assets/images/reward_tile.png",
+    arrowUp: "./assets/images/arrow_up.png",
+    arrowDown: "./assets/images/arrow_down.png",
+    arrowLeft: "./assets/images/arrow_left.png",
+    arrowRight: "./assets/images/arrow_right.png"
 }
